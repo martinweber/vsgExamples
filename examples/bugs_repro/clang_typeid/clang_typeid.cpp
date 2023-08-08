378fa7ac6300620047fbcbe86ad708eeffb315b3
Martin Weber <martin@cgrebel.com>
2023-08-08 10:46:50 +0200

    printing new type_hash() value and using it to compare for compatibility

--- examples/bugs_repro/clang_typeid/clang_typeid.cpp
+++ examples/bugs_repro/clang_typeid/clang_typeid.cpp
@@ -7,10 +7,17 @@ int main(int argc, char** argv)
     auto local = vsg::MatrixTransform::create();
     auto dylib = create_object_in_dylib();
 
-    std::cout << "Local: type name: " << local->type_info().name() << ", type hash: " << local->type_info().hash_code() << std::endl;
-    std::cout << "Dylib: type name: " << dylib->type_info().name() << ", type hash: " << dylib->type_info().hash_code() << std::endl;
+    std::cout << "Local: type name: " << std::setw(32) << std::left << local->type_info().name()
+              << ", type hash: (type_info) " << std::setw(12) << local->type_info().hash_code()
+              << " | (FNV-1a) " << local->type_hash()
+              << std::endl;
 
-    std::cout << (dylib->is_compatible(local->type_info()) ? "types are compatible" : "types are not compatible") << std::endl;
+    std::cout << "Dylib: type name: " << std::setw(32) << std::left << dylib->type_info().name()
+              << ", type hash: (type_info) " << std::setw(12) << dylib->type_info().hash_code()
+              << " | (FNV-1a) " << dylib->type_hash()
+              << std::endl;
+
+    std::cout << (dylib->is_compatible(local->type_hash()) ? "types are compatible" : "types are not compatible") << std::endl;
 
     // this dynamic_cast will result in the following clang_typeid runtime error when executed with debugger attached:
     // clang_typeid[30194:379372] dynamic_cast error 2: One or more of the following type_info's has hidden visibility or is defined
