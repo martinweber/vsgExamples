13c0e08f18db5a62c004b40c8d829517cb043cff
Martin Weber <martin@cgrebel.com>
2023-09-06 16:49:53 +0200

    added dynamic_cast<> to demonstrate Clang runtime error message (when debugger attached)

--- examples/bugs_repro/clang_typeid/clang_typeid.cpp
+++ examples/bugs_repro/clang_typeid/clang_typeid.cpp
@@ -12,5 +12,11 @@ int main(int argc, char** argv)
 
     std::cout << (dylib->is_compatible(local->type_info()) ? "types are compatible" : "types are not compatible") << std::endl;
 
+    // this dynamic_cast will result in the following clang_typeid runtime error when executed with debugger attached:
+    // clang_typeid[30194:379372] dynamic_cast error 2: One or more of the following type_info's has hidden visibility or is defined
+    // in more than one translation unit. They should all have public visibility. N3vsg6ObjectE, N3vsg15MatrixTransformE, N3vsg15MatrixTransformE.
+    auto matrix_transform_ptr = dynamic_cast<vsg::MatrixTransform*>(dylib.get());
+    assert(matrix_transform_ptr != nullptr);
+
     return EXIT_SUCCESS;
 }
